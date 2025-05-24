f4c8727810a0f87c3d4433937108e233414a3fba
romanalisoy <eldar.muzefferov@gmail.com>
2025-05-25 01:51:33 +0400

    refactor: update configuration files for improved defaults and clarity

---
+++ config/league.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'teams' => [
+        ['name'=>'Chelsea',          'strength'=>90],
+        ['name'=>'Arsenal',          'strength'=>85],
+        ['name'=>'Manchester City',  'strength'=>88],
+        ['name'=>'Liverpool',        'strength'=>87],
+    ],
+];
