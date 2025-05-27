f6bc87c51639e06bd4238b90ca8a228478db2d2d
romanalisoy <eldar.muzefferov@gmail.com>
2025-05-28 03:53:27 +0400

    feat: add method to calculate team statistics

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -28,4 +28,46 @@ class Team extends Model
         return $this->hasMany(Game::class, 'away_team_id');
     }
 
+    public function calculateStats(): array
+    {
+        $played = $this->homeGames->merge($this->awayGames);
+        $stats = [
+            'team_id'  => $this->id,
+            'team_name'=> $this->name,
+            'played'   => $played->count(),
+            'won'      => 0,
+            'drawn'    => 0,
+            'lost'     => 0,
+            'for'      => 0,
+            'against'  => 0,
+            'gd'       => 0,
+            'points'   => 0,
+        ];
+
+        foreach ($played as $game) {
+            if ($game->home_team_id === $this->id) {
+                $f  = $game->home_score;
+                $ag = $game->away_score;
+            } else {
+                $f  = $game->away_score;
+                $ag = $game->home_score;
+            }
+            $stats['for']     += $f;
+            $stats['against'] += $ag;
+
+            if ($f > $ag) {
+                $stats['won']++;
+                $stats['points'] += 3;
+            } elseif ($f === $ag) {
+                $stats['drawn']++;
+                $stats['points'] += 1;
+            } else {
+                $stats['lost']++;
+            }
+        }
+
+        $stats['gd'] = $stats['for'] - $stats['against'];
+        return $stats;
+    }
+
 }
